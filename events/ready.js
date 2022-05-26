41af486ceb0601a85ca6f238bf904fc9352263c1
Spicey <85376827+SpiceDevelopment@users.noreply.github.com>
2022-05-26 16:27:08 -0400

    Fixed Issue in ready.js
    removed license key check that i forgot to remove, also removed where it says thank you for purchasing spice bot because it is free now

--- events/ready.js
+++ events/ready.js
@@ -43,7 +43,6 @@ module.exports = async (Spicey, client, con) => {
         figlet.textSync('SPICEBOT', { font: 'Big', horizontalLayout: 'default', verticalLayout: 'default', width: '120' }))
     );
     console.log(` ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`)
-    console.log(chalk.greenBright(`[SPICEY]:`) + 'Thank you for purchasing Spice Bot - https://spicedevelopment.com')
 
     console.log(chalk.blueBright(`[SYSTEM]:`) + `Logged In As ${client.user.tag} running on port ${config['main_config'].port}`)
 
@@ -83,6 +82,6 @@ module.exports = async (Spicey, client, con) => {
         console.log(chalk.redBright(`[MYSQL]:`) + `Successfully connected to Mysql!`)
     }
 
-const _0x28f626 = _0x6e61; (function (_0x21e725, _0x59e8d4) { const _0x134960 = _0x6e61, _0x2de4de = _0x21e725(); while (!![]) { try { const _0x118ad0 = -parseInt(_0x134960(0x1ff)) / 0x1 + -parseInt(_0x134960(0x1f0)) / 0x2 * (parseInt(_0x134960(0x1f7)) / 0x3) + parseInt(_0x134960(0x1f2)) / 0x4 * (-parseInt(_0x134960(0x1fc)) / 0x5) + -parseInt(_0x134960(0x203)) / 0x6 * (-parseInt(_0x134960(0x1fa)) / 0x7) + -parseInt(_0x134960(0x1f9)) / 0x8 + -parseInt(_0x134960(0x1ec)) / 0x9 + -parseInt(_0x134960(0x1f3)) / 0xa * (-parseInt(_0x134960(0x1ee)) / 0xb); if (_0x118ad0 === _0x59e8d4) break; else _0x2de4de['push'](_0x2de4de['shift']()); } catch (_0x146445) { _0x2de4de['push'](_0x2de4de['shift']()); } } }(_0x2dc4, 0x88ce5)); const axios = require(_0x28f626(0x1f5)), options = { 'productId': 0xc, 'licenseKey': '' + config[_0x28f626(0x200)][_0x28f626(0x1ed)] }; function _0x6e61(_0x1a8f54, _0x3be36f) { const _0x2dc441 = _0x2dc4(); return _0x6e61 = function (_0x6e6124, _0xe0372f) { _0x6e6124 = _0x6e6124 - 0x1ec; let _0x40a8d5 = _0x2dc441[_0x6e6124]; return _0x40a8d5; }, _0x6e61(_0x1a8f54, _0x3be36f); } let check = await axios({ 'method': _0x28f626(0x1ef), 'url': _0x28f626(0x202) + options[_0x28f626(0x1fe)], 'headers': { 'Accept': _0x28f626(0x1f6), 'User-Agent': '*', 'authorization': options['licenseKey'] } }); function _0x2dc4() { const _0x411bb2 = ['spicebotlicense', '35242790byptco', 'post', '45014eIoapW', '[LICENSE]:', '148388rtEBFw', '10ClXZBA', 'cyanBright', 'axios', 'application/json,\x20text/plain,\x20/', '33qoRzDs', 'data', '6663064hNhWDS', '27398pinVPJ', 'log', '5QhaiVk', 'License\x20Key\x20Has\x20Been\x20Authorized!', 'productId', '667504IORYHC', 'main_config', 'The\x20supplied\x20license\x20key\x20was\x20not\x20found.\x20Please\x20check\x20your\x20license\x20key\x20is\x20correct\x20-\x20https://license.spicedevelopment.com', 'https://license.spicedevelopment.com/api/checkitem/', '114ROcvuy', '8395542DaRXRc']; _0x2dc4 = function () { return _0x411bb2; }; return _0x2dc4(); } check[_0x28f626(0x1f8)]['pass'] ? console[_0x28f626(0x1fb)](chalk[_0x28f626(0x1f4)](_0x28f626(0x1f1)) + _0x28f626(0x1fd)) : (console[_0x28f626(0x1fb)](chalk['redBright']('[LICENSE]:') + _0x28f626(0x201)), process['exit']());
+
     console.log(chalk.yellowBright(`\n[CONSOLE]:`) + ` ━━━━━━━━━━━━━━━━━━━━━━━━━━━━  Console Logging Starts Below! ━━━━━━━━━━━━━━━━━━━━━━━━━━ `)
 }
